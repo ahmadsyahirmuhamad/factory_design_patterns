c60aac8c6fcc09c9469999d3df867eda56318e2a
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2016-12-15 03:27:34 +0800

    main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,13 @@
-print "hello world"
+require_relative 'sound_factory'
+
+
+sound_factory = SoundFactory.new()
+
+person = sound_factory.get_sound("Person")
+person.sound
+
+cat = sound_factory.get_sound("Cat")
+cat.sound
+
+dog = sound_factory.get_sound("Dog")
+dog.sound
